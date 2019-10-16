f21454e1c28c869411ca01541e54430512e5a0b8
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2019-10-16 13:17:55 +0300

    new branch and css

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import MessageContainer from './components/Message/MessageContainer';
 import Navbar from './components/Navbar/Navbar';
 import { Route, BrowserRouter } from 'react-router-dom';
 import store from './redux/redux-store';
+import s from './App.module.css';
 import {Provider} from 'react-redux';
 import UsersContainer from './components/UsersContainer/UsersContainer';
 import MessageLocalState from './components/MessageLocalState/MessageLocalState';
 
 class App extends React.Component {
   render() {
-    return <div>
+    return <div className={s.App}>
       <Navbar />
       <div>
         <Route path='/messages'
