9715539ebd45e7a016ea7f82534409b94f14c98f
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2019-10-17 23:05:23 +0300

    local state

--- src/components/MessageLocalState/MessageLocalState.jsx
+++ src/components/MessageLocalState/MessageLocalState.jsx
@@ -8,11 +8,38 @@ class MessageLocalState extends React.Component {
             { id: '2', text: 'two' },
             { id: '3', text: 'three' },
             { id: '4', text: 'four' }
-        ]
+        ],
+        textareaValue: '123'
     }
+
+    onAddMessage = () => {
+        const newItem = {
+            text: this.state.textareaValue,
+            id: Date.now()
+          };
+
+        this.setState ({
+            // messages: [this.messages, {id: 5, text: this.state.textareaValue }]
+            messages: this.state.messages.concat(newItem)
+        })
+    }
+    onChangeMessage = (e) => {
+        this.setState ({
+            textareaValue: e.currentTarget.value
+        })
+    }
+
     render() {
+        let newText = this.state.textareaValue;
+
         return <div>
-            {this.state.messages.map(t => <NewItemMessage key={t.id} message={t}/>)}
+            {this.state.messages.map(t => <NewItemMessage key={t.id} message={t} />)}
+            <div>
+                <textarea value={newText} onChange={this.onChangeMessage}/>
+            </div>
+            <div>
+                <button onClick={this.onAddMessage}>push</button>
+            </div>
         </div>
     }
 }
--- src/trainigtList.js
+++ src/trainigtList.js
@@ -1,16 +1,53 @@
 import React from 'react';
 
-const Users = props => {
-    return <div>
-        {
-            props.users.map(u => <User key={u.id} user={u} />)
-        }
-    </div>
-}
-export default Users;
 
-const User = props => {
+class MessageLocalState extends React.Component {
+    state = {
+        messages: [
+            { id: '1', text: 'one' },
+            { id: '2', text: 'two' },
+            { id: '3', text: 'three' },
+            { id: '4', text: 'four' }
+        ],
+        textareaValue: '123'
+    }
+
+    onAddMessage = (textareaValue) => {
+
+        // console.log(e)
+        this.setState ({
+            
+            messages: [this.state.messages.concat(), { id: '5', text: textareaValue }]
+        })
+    }
+    onChangeMessage = (e) => {
+
+        this.setState ({
+            textareaValue: e.currentTarget.value
+        })
+    }
+
+    render() {
+        let newText = this.state.textareaValue;
+
+        return <div>
+            {this.state.messages.map(t => <NewItemMessage key={t.id} message={t} />)}
+            <div>
+                <textarea value={newText} onChange={this.onChangeMessage}/>
+            </div>
+            <div>
+                <button onClick={this.onAddMessage}>push</button>
+            </div>
+        </div>
+    }
+}
+export default MessageLocalState;
+
+
+const NewItemMessage = props => {
     return <div>
-        {props.user.name}
+        {props.message.text}
     </div>
 }
+
+
